aec30363fa161f933e84512c620aecaefdb07d25
strBCL1 <work.artem.popov@gmail.com>
2022-06-09 19:55:30 +0200

    Initial commit - add 'Flyway', 'MapStruct' and 'Swagger 2' to Spring Boot

---
+++ src/main/java/com/example/libraryprojectv2/LibraryProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.libraryprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryProjectV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraryprojectv2/LibraryProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
