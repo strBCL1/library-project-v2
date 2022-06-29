4d20d476b7f0816cddf0718e045e17da586683cc
strBCL1 <work.artem.popov@gmail.com>
2022-06-30 01:11:24 +0200

    Implement valid 'PUT of author'

--- src/test/java/com/example/libraryprojectv2/domain/author/controller/AuthorControllerTest.java
+++ src/test/java/com/example/libraryprojectv2/domain/author/controller/AuthorControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.libraryprojectv2.domain.author.controller;
 
 import com.example.libraryprojectv2.domain.author.dao.AuthorRepository;
+import com.example.libraryprojectv2.domain.author.dto.AuthorDataDto;
 import com.example.libraryprojectv2.domain.author.dto.AuthorIdDto;
 import com.example.libraryprojectv2.domain.author.model.Author;
 import com.example.libraryprojectv2.domain.author.service.AuthorService;
@@ -29,8 +30,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -361,6 +361,45 @@ class AuthorControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.cause", equalTo("Message: 'Author's last name may only contain up to 45 letters!', rejected value: '" + authorIdDto.getLastName() + "'")));
+    }
+
+
+//    ============================================== PUT author ====================================================
+
+
+    @Test
+    void givenValidAuthorDataDto_whenUpdateAuthorData_thenReturnUpdatedAuthorDto() throws Exception {
+        final Author author = new Author(VALID_ORCID_ID, VALID_FIRST_NAME, VALID_LAST_NAME, new HashSet<>());
+        final Book book = new Book(VALID_ISBN_ID, VALID_TITLE, new HashSet<>(), null);
+        final Publisher publisher = new Publisher(VALID_ID, VALID_NAME, VALID_ADDRESS, VALID_CITY, VALID_COUNTRY, new HashSet<>());
+
+        author.addBook(book);
+        book.updatePublisher(publisher);
+
+        final int repeatTimes = 2;
+        final AuthorDataDto authorDataDto = new AuthorDataDto(author.getFirstName().repeat(repeatTimes), author.getLastName().repeat(repeatTimes));
+        final Author updatedAuthor = new Author(author.getOrcidId(), authorDataDto.getFirstName(), authorDataDto.getLastName(), author.getBooks());
+
+        when(authorRepository.findById(anyString())).thenReturn(Optional.of(author));
+        when(authorRepository.save(any(Author.class))).thenReturn(updatedAuthor);
+
+        mockMvc.perform(put("/authors/" + author.getOrcidId())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(authorDataDto)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.orcidId", equalTo(author.getOrcidId())))
+                .andExpect(jsonPath("$.firstName", equalTo(author.getFirstName())))
+                .andExpect(jsonPath("$.lastName", equalTo(author.getLastName())))
+                .andExpect(jsonPath("$.books", hasSize(1)))
+                .andExpect(jsonPath("$.books.[0].isbnId", equalTo(book.getIsbnId())))
+                .andExpect(jsonPath("$.books.[0].title", equalTo(book.getTitle())))
+                .andExpect(jsonPath("$.books.[0].publisher.id", equalTo(publisher.getId().intValue())))
+                .andExpect(jsonPath("$.books.[0].publisher.name", equalTo(publisher.getName())))
+                .andExpect(jsonPath("$.books.[0].publisher.address", equalTo(publisher.getAddress())))
+                .andExpect(jsonPath("$.books.[0].publisher.city", equalTo(publisher.getCity())))
+                .andExpect(jsonPath("$.books.[0].publisher.country", equalTo(publisher.getCountry())))
+                .andExpect(jsonPath("$.books.[0].publisher.books").doesNotExist());
     }
 
 
