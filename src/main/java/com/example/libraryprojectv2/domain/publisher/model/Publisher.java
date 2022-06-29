b74ec2b448097c2445b7d83b6c2025e2a6f05695
strBCL1 <work.artem.popov@gmail.com>
2022-06-29 14:50:10 +0200

    Add validation of 'Publisher' entity

--- src/main/java/com/example/libraryprojectv2/domain/publisher/model/Publisher.java
+++ src/main/java/com/example/libraryprojectv2/domain/publisher/model/Publisher.java
@@ -4,6 +4,10 @@ import com.example.libraryprojectv2.domain.book.model.Book;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -20,20 +24,26 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "publisher_generator")
     @SequenceGenerator(name = "publisher_generator", sequenceName = "publisher_id_seq", allocationSize = 1)
+    @Positive
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 45)
+    @Pattern(regexp = "[a-zA-Z]{0,45}", message = "Publisher's name may only contain up to 45 letters!")
     private String name;
 
     @Column(name = "address")
+    @Size(max = 255, message = "Publisher's address may only contain up to 255 characters!")
     private String address;
 
-    @Column(name = "city")
+    @Column(name = "city", length = 45)
+    @Size(max = 45, message = "Publisher's city may only contain up to 45 characters!")
     private String city;
 
-    @Column(name = "country")
+    @Column(name = "country", length = 45)
+    @Size(max = 45, message = "Publisher's country may only contain up to 45 characters!")
     private String country;
 
+    @Valid
     @OneToMany(cascade = {
             CascadeType.MERGE,
             CascadeType.PERSIST,
